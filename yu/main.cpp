34b61cb4ccfaecd56d3f4c51669c1fca9ce3345a
小鱼🐟 <20141105060w@AiMac.local>
2015-11-04 18:41:17 +0800

    Initial Commit

---
+++ yu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  yu
+//
+//  Created by 小鱼🐟 on 15/11/4.
+//  Copyright (c) 2015年 小鱼🐟. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
