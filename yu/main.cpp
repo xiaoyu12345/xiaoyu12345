ea535f25d84e8320949713a9812e87440899aa57
xiaoyu12345 <20141105060w@Mac12.local>
2015-11-11 19:14:49 +0800

    sadx
    asxasxsa

--- yu/main.cpp
+++ yu/main.cpp
@@ -10,6 +10,29 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int *a;
+    int i,j,temp;
+    int N;
+    scanf("%d",&N);
+    a=(int *)malloc(N*4);
+    if(a==0)
+        exit(0);
+    for(i=0;i<N;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<N;i++)
+        printf("%d\n",a[i]);
+    free(a);
     return 0;
 }
